c13a0e0d99ab5d41d7cef8bee3c9be27c0cbfb4e
augustinandrea <augustin.andrea@gmail.com>
2021-08-04 20:55:25 -0700

    I'm very confused with React right now so am making multiple folders and trying to create the header and navbar components.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+//import React, { useState, useEffect } from "react";
+//import logo from './logo.svg';
 import './App.css';
+import Navbar from "./Components/Navbar";
+//import Home from "./Components/Home/Home";
+import About from "./Components/About/About";
+import Contact from "./Components/Contact/index";
+//import Projects from "./Components/Projects/Projects";
+//import Footer from "./Components/Footer";
+//import Resume from "./Components/Resume/Resume";
 
 function App() {
+  /*const [load, upadateLoad] = useState(true);
+  
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      upadateLoad(false);
+    }, 1200);
+    
+    return () => clearTimeout(timer);
+  }, []);*/
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar
+
+      ></Navbar>
+      <main>
+    
+          <>
+            <About></About>
+          </>
+        ) : (
+          <Contact></Contact>
+        )
+      </main>
     </div>
   );
 }
